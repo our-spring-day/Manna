230caa2e8a8b7c5c357136a4a48434593df9bfdd
wswon <dnjsdntjr1@gmail.com>
2020-08-08 00:41:28 +0900

    feat(...) : ext file 추가 - extension 은 적절한 사용하기

---
+++ app/src/main/java/com/manna/ext/ViewExt.kt
@@ -0,0 +1,22 @@
+package com.manna.ext
+
+import android.view.View
+import androidx.core.view.isGone
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("android:visibleIf")
+fun View.setVisibleIf(value: Boolean) {
+    isVisible = value
+}
+
+@BindingAdapter("android:invisibleIf")
+fun View.setInvisibleIf(value: Boolean) {
+    isInvisible = value
+}
+
+@BindingAdapter("android:goneIf")
+fun View.setGoneIf(value: Boolean) {
+    isGone = value
+}
