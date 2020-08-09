b91b18ca9427f24d4021511dba2f7a0d64bf4f88
wswon <dnjsdntjr1@gmail.com>
2020-08-09 22:45:11 +0900

    feat(...) : 약속상세화면 bottom sheet 기능 구현

--- app/src/main/java/com/manna/view/MeetDetailActivity.kt
+++ app/src/main/java/com/manna/view/MeetDetailActivity.kt
@@ -1,12 +1,61 @@
 package com.manna.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 import com.manna.R
+import com.manna.ext.ViewUtil
+import kotlinx.android.synthetic.main.activity_meet_detail.*
 
 class MeetDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meet_detail)
+
+        ViewUtil.setStatusBarTransparent(this)
+
+        top_panel.fitsSystemWindows = true
+
+        btn_back.setOnClickListener {
+            onBackPressed()
+        }
+
+        btn_menu.setOnClickListener {
+
+        }
+
+        BottomSheetBehavior.from(bottom_sheet)
+            .addBottomSheetCallback(createBottomSheetCallback(bottom_sheet_state))
     }
+
+    private fun createBottomSheetCallback(text: TextView): BottomSheetCallback =
+        object : BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+                text.text = when (newState) {
+                    BottomSheetBehavior.STATE_DRAGGING -> "STATE DRAGGING"
+                    BottomSheetBehavior.STATE_EXPANDED -> "STATE EXPANDED"
+                    BottomSheetBehavior.STATE_COLLAPSED -> "STATE COLLAPSED"
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                        String.format(
+                            "STATE_HALF_EXPANDED\\nhalfExpandedRatio = %.2f",
+                            BottomSheetBehavior.from(bottomSheet).halfExpandedRatio
+                        )
+                    }
+                    else -> {
+                        text.text.toString()
+                    }
+                }
+            }
+
+            override fun onSlide(
+                bottomSheet: View,
+                slideOffset: Float
+            ) {
+            }
+        }
 }
