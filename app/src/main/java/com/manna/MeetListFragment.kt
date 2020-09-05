f1002b45e43e53d76be56bbecc94c2ea5e496324
josh <josh@watcha.com>
2020-09-05 10:29:41 +0900

    feat(...) : 메인 화면 탭에 쓰일 Fragment init

---
+++ app/src/main/java/com/manna/MeetListFragment.kt
@@ -0,0 +1,28 @@
+package com.manna
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.manna.databinding.FragmentMeetListBinding
+
+class MeetListFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentMeetListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_meet_list, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
