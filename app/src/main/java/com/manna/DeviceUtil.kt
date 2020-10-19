e8db83479d5a6e62118ffa4abe95adbe9f2585f4
josh <josh@watcha.com>
2020-10-20 01:10:21 +0900

    feat(DeviceUtil) : 디바이스 고유식별자 Util

---
+++ app/src/main/java/com/manna/DeviceUtil.kt
@@ -0,0 +1,14 @@
+package com.manna
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.provider.Settings
+
+
+object DeviceUtil {
+
+    @SuppressLint("HardwareIds")
+    fun getAndroidID(context: Context): String {
+        return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+    }
+}
