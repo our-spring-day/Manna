31fea424688c9b7b1674390e046f215f0de67918
josh <josh@watcha.com>
2020-10-19 22:29:38 +0900

    feat(...) : websocket test

---
+++ app/src/main/java/com/manna/view/WebSocketTestActivity.kt
@@ -0,0 +1,91 @@
+package com.manna.view
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.manna.Logger
+import com.manna.R
+import io.crossbar.autobahn.websocket.WebSocketConnection
+import io.crossbar.autobahn.websocket.exceptions.WebSocketException
+import io.crossbar.autobahn.websocket.interfaces.IWebSocketConnectionHandler
+import io.crossbar.autobahn.websocket.types.ConnectionResponse
+
+class WebSocketTestActivity : AppCompatActivity() {
+
+    private val mConnection: WebSocketConnection = WebSocketConnection()
+    private lateinit var inputView: EditText
+    private lateinit var outputView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_websocket)
+
+        inputView = findViewById(R.id.input)
+        outputView = findViewById(R.id.output_window)
+        findViewById<Button>(R.id.send_button).setOnClickListener {
+            mConnection.sendMessage(inputView.text.toString())
+        }
+        start()
+    }
+
+    private fun start() {
+        val wsuri = "wss://echo.websocket.org"
+        try {
+            mConnection.connect(wsuri, object : IWebSocketConnectionHandler {
+
+                override fun onMessage(payload: ByteArray?, isBinary: Boolean) {
+                    Logger.d("$payload $isBinary")
+                }
+
+                override fun onConnect(response: ConnectionResponse?) {
+                    Logger.d("$response")
+                }
+
+                override fun onPing() {
+                    Logger.d("")
+                }
+
+                override fun onPing(payload: ByteArray?) {
+                    Logger.d("$payload")
+                }
+
+                override fun onPong() {
+                    Logger.d("")
+                }
+
+                override fun onPong(payload: ByteArray?) {
+                    Logger.d("$payload")
+                }
+
+                override fun setConnection(connection: WebSocketConnection?) {
+                    Logger.d("$connection")
+                }
+
+                @SuppressLint("SetTextI18n")
+                override fun onOpen() {
+                    Logger.d("Status: Connected to $wsuri")
+                    outputView.text = outputView.text.toString() + "\nconnected to " + wsuri
+                }
+
+                override fun onMessage(payload: String?) {
+                    Logger.d("Got echo: $payload")
+                    outputView.text = outputView.text.toString() + "\nGot echo: " + payload
+                }
+
+                override fun onClose(code: Int, reason: String?) {
+                    Logger.d("Connection lost.")
+                    outputView.text = outputView.text.toString() + "\nConnection lost: " + reason
+                }
+            })
+        } catch (e: WebSocketException) {
+            Logger.d(e.toString())
+        }
+    }
+
+    companion object {
+        private const val TAG = "WebSocketTestActivity"
+    }
+}
