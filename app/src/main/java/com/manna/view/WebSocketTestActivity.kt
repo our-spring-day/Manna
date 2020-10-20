a60d2fd2eb8191532b3a75a886dc1b49f3a5aa02
josh <josh@watcha.com>
2020-10-21 01:42:00 +0900

    stash

--- app/src/main/java/com/manna/view/WebSocketTestActivity.kt
+++ app/src/main/java/com/manna/view/WebSocketTestActivity.kt
@@ -9,8 +9,10 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.JsonObject
 import com.manna.Logger
 import com.manna.R
+import com.manna.network.api.MeetApi
 import com.manna.network.model.meet.MeetResponseItem
 import io.crossbar.autobahn.websocket.WebSocketConnection
 import io.crossbar.autobahn.websocket.exceptions.WebSocketException
@@ -29,18 +31,30 @@ class WebSocketTestActivity : AppCompatActivity() {
 
         val meetItem = intent.getParcelableExtra<MeetResponseItem>(MEET_ITEM)
 
-        Toast.makeText(this, meetItem.toString(), Toast.LENGTH_LONG).show()
+        Toast.makeText(this, meetItem?.toString(), Toast.LENGTH_LONG).show()
 
         inputView = findViewById(R.id.input)
         outputView = findViewById(R.id.output_window)
         findViewById<Button>(R.id.send_button).setOnClickListener {
-            mConnection.sendMessage(inputView.text.toString())
+
+            val jsonObject = JsonObject().apply {
+                addProperty("sender", "우석")
+                addProperty("message", "우석메시지")
+            }
+
+            val jsonString = JsonObject().apply {
+                add("chatToServer", jsonObject)
+            }
+            Logger.d("$jsonString")
+
+            mConnection.sendMessage(jsonString.toString())
         }
         start()
     }
 
     private fun start() {
-        val wsuri = "wss://echo.websocket.org"
+
+        val wsuri = "ws://ec2-3-34-179-87.ap-northeast-2.compute.amazonaws.com:9999/chat/chatToServer"//""ws://ec2-13-124-151-24.ap-northeast-2.compute.amazonaws.com:9999/manna"
         try {
             mConnection.connect(wsuri, object : IWebSocketConnectionHandler {
 
