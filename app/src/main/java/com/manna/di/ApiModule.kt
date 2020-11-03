efe98184a5856f323e19d9e91f6a9f6e6bd4e573
josh <josh@watcha.com>
2020-11-04 00:35:37 +0900

    feat(...) : 통신 로그를 추가한다.

--- app/src/main/java/com/manna/di/ApiModule.kt
+++ app/src/main/java/com/manna/di/ApiModule.kt
@@ -7,10 +7,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(ApplicationComponent::class)
@@ -30,10 +33,15 @@ object ApiModule {
     @Singleton
     @Provides
     fun provideBingApi(): BingApi {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         return Retrofit.Builder()
             .baseUrl(BingApi.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(client)
             .build()
             .create(BingApi::class.java)
     }
