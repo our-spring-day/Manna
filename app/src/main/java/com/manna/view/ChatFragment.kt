182603c9f224f775e5f1768ac2b6a82619fdb9cc
josh <josh@watcha.com>
2020-11-06 04:04:32 +0900

    fix(ChatFragment) : edittext focus처리

--- app/src/main/java/com/manna/view/ChatFragment.kt
+++ app/src/main/java/com/manna/view/ChatFragment.kt
@@ -6,16 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.manna.Logger
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.manna.R
 import com.manna.databinding.FragmentChatBinding
 import com.manna.ext.HeightProvider
 import com.manna.ext.ViewUtil
+import kotlinx.android.synthetic.main.activity_meet_detail.*
 
 
 class ChatFragment : Fragment() {
 
     private lateinit var binding: FragmentChatBinding
+    private var keyboardHeight = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,18 +32,59 @@ class ChatFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val behavior = BottomSheetBehavior.from(requireActivity().bottom_sheet)
+
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        if (keyboardHeight > 0) {
+                            inputViewTransY(
+                                ViewUtil.getScreenHeightPixels(activity) - keyboardHeight - ViewUtil.convertDpToPixel(
+                                    requireContext(),
+                                    12f
+                                ).toInt()
+                            )
+                        } else {
+                            (activity as MeetDetailActivity).resetBottomSheet(1f)
+                        }
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        binding.inputChat.clearFocus()
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+        })
+
+        binding.inputChat.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                if (behavior.state != BottomSheetBehavior.STATE_EXPANDED) {
+                    behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                }
+            }
+        }
+
         HeightProvider(requireActivity()).init()
             .setHeightListener { height ->
-                Logger.d("$height")
+                keyboardHeight = height
 
                 if (height > 0) {
-                    inputViewTransY(ViewUtil.getScreenHeightPixels(activity) - height - ViewUtil.convertDpToPixel(requireContext(), 12f).toInt())
+                    inputViewTransY(
+                        ViewUtil.getScreenHeightPixels(activity) - height - ViewUtil.convertDpToPixel(
+                            requireContext(),
+                            12f
+                        ).toInt()
+                    )
                 } else {
                     (activity as MeetDetailActivity).resetBottomSheet(1f)
+                    binding.inputChat.clearFocus()
                 }
             }
     }
-
 
 
     fun inputViewTransY(y: Int) {
