e69467518ff135ccc0419978e43db190596ba3f2
josh <josh@watcha.com>
2020-11-06 23:24:03 +0900

    fix(ChatFragment) : socket 붙임

--- app/src/main/java/com/manna/view/ChatFragment.kt
+++ app/src/main/java/com/manna/view/ChatFragment.kt
@@ -7,17 +7,44 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.manna.Logger
 import com.manna.R
 import com.manna.databinding.FragmentChatBinding
 import com.manna.ext.HeightProvider
 import com.manna.ext.ViewUtil
+import io.socket.client.IO
+import io.socket.client.Manager
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
 import kotlinx.android.synthetic.main.activity_meet_detail.*
+import java.net.URI
 
 
 class ChatFragment : Fragment() {
 
     private lateinit var binding: FragmentChatBinding
     private var keyboardHeight = 0
+    private lateinit var chatSocket: Socket
+
+    private val onChatConnectReceiver = Emitter.Listener { args ->
+        activity?.runOnUiThread {
+            Logger.d("${args.map { it.toString() }}")
+        }
+    }
+
+
+    private val onChatReceiver = Emitter.Listener { args ->
+        activity?.runOnUiThread {
+            Logger.d("${args.map { it.toString() }}")
+        }
+    }
+
+    override fun onDestroy() {
+        chatSocket.disconnect()
+        chatSocket.off(CHAT_CONNECT, onChatReceiver)
+        super.onDestroy()
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,7 +59,36 @@ class ChatFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.ddaBong.setOnClickListener {
+            chatSocket.emit(CHAT_MESSAGE, binding.inputChat.text.toString())
+        }
+
+
         val behavior = BottomSheetBehavior.from(requireActivity().bottom_sheet)
+
+        val options = IO.Options()
+        options.query = "mannaID=96f35135-390f-496c-af00-cdb3a4104550&deviceToken=f606564d8371e455"
+
+        val chatManager = Manager(URI("https://manna.duckdns.org:19999"), options)
+        chatSocket =
+            chatManager.socket("/chat").apply {
+                on(CHAT_CONNECT, onChatConnectReceiver)
+                on(CHAT_MESSAGE, onChatReceiver)
+
+                on(Socket.EVENT_CONNECT) {
+                    Logger.d("EVENT_CONNECT ${it.map { it.toString() }}")
+                }
+
+                on(Socket.EVENT_DISCONNECT) {
+                    Logger.d("EVENT_DISCONNECT ${it.map { it.toString() }}")
+                }
+
+                on(Socket.EVENT_MESSAGE) {
+                    Logger.d("EVENT_MESSAGE ${it.map { it.toString() }}")
+                }
+
+                connect()
+            }
 
         behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
@@ -95,6 +151,9 @@ class ChatFragment : Fragment() {
     }
 
     companion object {
+        private const val CHAT_CONNECT = "chatConnect"
+        private const val CHAT_MESSAGE = "chat"
+
         fun newInstance() = ChatFragment()
     }
 }
