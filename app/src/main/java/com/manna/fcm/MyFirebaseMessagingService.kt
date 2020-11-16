60a84aea3c5c86dbfbfb57ef7b5f66c90fc335fa
josh <josh@watcha.com>
2020-11-16 23:25:25 +0900

    feat(MyFirebaseMessagingService.kt) : 여기서 foreground notification 처리 추가해야한다. newToken 호출시 토큰 server에 refresh도

---
+++ app/src/main/java/com/manna/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,40 @@
+package com.manna.fcm
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        val TAG = "FCM"
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+//                scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+//                handleNow()
+            }
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+    }
+}
