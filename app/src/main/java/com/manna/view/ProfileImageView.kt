823892f6ec817a96e824def959896646a417286e
josh <josh@watcha.com>
2020-12-04 03:00:49 +0900

    feat(ProfileImageView) : xml shape 들 없애고 요방법으로 밀어보자.

--- app/src/main/java/com/manna/view/ProfileImageView.kt
+++ app/src/main/java/com/manna/view/ProfileImageView.kt
@@ -2,11 +2,13 @@ package com.manna.view
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
@@ -28,6 +30,13 @@ class ProfileImageView @JvmOverloads constructor(
             val a = context.obtainStyledAttributes(attrs, R.styleable.ProfileImageView)
             cornerRadius = a.getDimension(R.styleable.ProfileImageView_corner_radius, 0f)
             borderDrawable = a.getDrawable(R.styleable.ProfileImageView_border_res)
+
+            if (borderDrawable == null) {
+                borderDrawable = GradientDrawable().apply {
+                    this.cornerRadius = this@ProfileImageView.cornerRadius
+                    this.setStroke(ViewUtil.convertDpToPixel(context, 2f).toInt(), ContextCompat.getColor(context, R.color.ffdadada))
+                }
+            }
             a.recycle()
         }
         view = inflate(context, R.layout.view_profile_image, this) as ViewGroup
