6f08f3b70f2b891909a75d9bad4808da92a6fc1a
LEEYEONJAE <eunoiaa22@gmail.com>
2020-12-23 02:49:17 +0900

    feat(...) : 프로필 사진 가이드 화면 수정

--- app/src/main/java/com/manna/presentation/sign_up/ProfileGuideFragment.kt
+++ app/src/main/java/com/manna/presentation/sign_up/ProfileGuideFragment.kt
@@ -1,11 +1,14 @@
 package com.manna.presentation.sign_up
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import com.manna.R
 import com.manna.common.BaseFragment
 import com.manna.databinding.FragmentProfileGuideBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class ProfileGuideFragment :
@@ -14,6 +17,26 @@ class ProfileGuideFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.ivBack.setOnClickListener {
+
+        }
+
+        binding.tvAlbum.setOnClickListener {
+            (activity as SignUpActivity).replace(ProfileConfirmFragment())
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(
+            true
+        ) {
+            override fun handleOnBackPressed() {
+                (activity as SignUpActivity).remove(ProfileGuideFragment())
+                (activity as SignUpActivity).replace(CreateNameFragment())
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
     }
 
     companion object {
