b4953667a1e343740428de8504950f876a6779aa
LEEYEONJAE <eunoiaa22@gmail.com>
2020-12-24 01:48:53 +0900

    feat(...) : 서비스 이용 약관 화면 수정

--- app/src/main/java/com/manna/presentation/sign_up/ServiceTermsFragment.kt
+++ app/src/main/java/com/manna/presentation/sign_up/ServiceTermsFragment.kt
@@ -1,30 +1,50 @@
 package com.manna.presentation.sign_up
 
+import android.content.Context
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
 import com.manna.R
 import com.manna.common.BaseFragment
 import com.manna.databinding.FragmentServiceTermsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class ServiceTermsFragment :
     BaseFragment<FragmentServiceTermsBinding>(R.layout.fragment_service_terms) {
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(
+            true
+        ) {
+            override fun handleOnBackPressed() {
+                (activity as SignUpActivity).remove(ServiceTermsFragment())
+                (activity as SignUpActivity).replace(ProfileConfirmFragment())
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.tvContent.movementMethod = ScrollingMovementMethod()
         binding.cbCheck.setOnClickListener {
             if (binding.cbCheck.isChecked) {
-                binding.tvNext.isEnabled = true
-                binding.tvNext.background =
+                binding.tvSignUp.isEnabled = true
+                binding.tvSignUp.background =
                     ContextCompat.getDrawable(requireContext(), R.drawable.bg_btn_blue)
             } else {
-                binding.tvNext.isEnabled = false
-                binding.tvNext.background =
+                binding.tvSignUp.isEnabled = false
+                binding.tvSignUp.background =
                     ContextCompat.getDrawable(requireContext(), R.drawable.bg_btn_gray)
             }
+        }
+        binding.tvSignUp.setOnClickListener {
+            requireActivity().finish()
         }
     }
 
