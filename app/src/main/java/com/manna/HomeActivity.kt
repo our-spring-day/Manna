39d8a7237f7d98a399164aa148d7261e5d87d865
josh <josh@watcha.com>
2021-01-09 02:52:16 +0900

    feat(...) : dialog 디테일 조금 더

--- app/src/main/java/com/manna/HomeActivity.kt
+++ app/src/main/java/com/manna/HomeActivity.kt
@@ -2,10 +2,12 @@ package com.manna
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
@@ -31,7 +33,26 @@ class SomeDialog : DialogFragment() {
         return binding.root
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tenDp = ViewUtil.convertDpToPixel(requireContext(), 10f)
+
+        binding.panel1.background = GradientDrawable().apply {
+            val array = floatArrayOf(tenDp, tenDp, tenDp, tenDp, 0f, 0f, 0f, 0f)
+            cornerRadii = array
+            setColor(Color.WHITE)
+        }
+
+        binding.panel2.background = GradientDrawable().apply {
+            val array = floatArrayOf(0f, 0f, 0f, 0f, tenDp, tenDp, tenDp, tenDp)
+            cornerRadii = array
+            setColor(ContextCompat.getColor(requireContext(), R.color.keyColor))
+        }
+    }
 }
+
 
 @AndroidEntryPoint
 class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
