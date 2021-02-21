fde384405a249bdd7face713c8e86364c6795fbd
yeonjae22 <eunoiaa22@gmail.com>
2021-02-21 19:25:14 +0900

    feat(...) : 회원탈퇴 화면에 하단 버튼 추가

--- app/src/main/java/com/manna/presentation/settings/DeleteAccountActivity.kt
+++ app/src/main/java/com/manna/presentation/settings/DeleteAccountActivity.kt
@@ -17,9 +17,10 @@ class DeleteAccountActivity :
         super.onCreate(savedInstanceState)
 
         binding.run {
-            layoutTitleBar.tvTitle.text = "탈퇴하려는 이유를\n알려주세요."
+            layoutTitleBar.tvTitle.text = getString(R.string.delete_account_reason)
+            layoutTitleBar.ivBack.setOnClickListener {
+                finish()
+            }
         }
-
-
     }
 }
